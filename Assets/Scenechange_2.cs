11b924e491d12428602fa3cce02d6cec87a18106
mitiasobi <ppple@wh.commufa.jp>
2024-11-14 06:16:07 +0900

    画面遷移追加
    画面遷移

---
+++ Assets/Scenechange_2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scenechange_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(osu);
+    }
+
+    // Update is called once per frame
+    void osu()
+    {
+        SceneManager.LoadScene("DriveResult");
+    }
+}
+
