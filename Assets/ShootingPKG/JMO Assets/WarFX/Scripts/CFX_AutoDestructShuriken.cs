43a205abf4af4912f0a7bc293ece34e00be61080
mitiasobi <ppple@wh.commufa.jp>
2025-08-04 09:40:13 +0900

    爆発時の効果音追加

--- Assets/ShootingPKG/JMO Assets/WarFX/Scripts/CFX_AutoDestructShuriken.cs
+++ Assets/ShootingPKG/JMO Assets/WarFX/Scripts/CFX_AutoDestructShuriken.cs
@@ -2,34 +2,42 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(ParticleSystem))]
+[RequireComponent(typeof(AudioSource))] // AudioSource を必須に
 public class CFX_AutoDestructShuriken : MonoBehaviour
 {
-	public bool OnlyDeactivate;
-	
-	void OnEnable()
-	{
-		StartCoroutine("CheckIfAlive");
-	}
-	
-	IEnumerator CheckIfAlive ()
-	{
-		while(true)
-		{
-			yield return new WaitForSeconds(0.5f);
-			if(!GetComponent<ParticleSystem>().IsAlive(true))
-			{
-				if(OnlyDeactivate)
-				{
-					#if UNITY_3_5
-						this.gameObject.SetActiveRecursively(false);
-					#else
-						this.gameObject.SetActive(false);
-					#endif
-				}
-				else
-					GameObject.Destroy(this.gameObject);
-				break;
-			}
-		}
-	}
+    public bool OnlyDeactivate;
+
+    void OnEnable()
+    {
+        // 効果音を再生
+        AudioSource audio = GetComponent<AudioSource>();
+        if (audio != null)
+        {
+            audio.Play();
+        }
+
+        StartCoroutine("CheckIfAlive");
+    }
+
+    IEnumerator CheckIfAlive()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            if (!GetComponent<ParticleSystem>().IsAlive(true))
+            {
+                if (OnlyDeactivate)
+                {
+#if UNITY_3_5
+                    this.gameObject.SetActiveRecursively(false);
+#else
+                    this.gameObject.SetActive(false);
+#endif
+                }
+                else
+                    GameObject.Destroy(this.gameObject);
+                break;
+            }
+        }
+    }
 }
